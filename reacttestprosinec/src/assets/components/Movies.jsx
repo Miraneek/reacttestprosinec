31ba020d800654c9fd74d326dcc0d231e3441416
Miraneek <surykata05@gmail.com>
2023-12-05 10:00:39 +0100

    idk

--- reacttestprosinec/src/assets/components/Movies.jsx
+++ reacttestprosinec/src/assets/components/Movies.jsx
@@ -1,16 +1,29 @@
-
 import {MovieCard} from "./MovieCard.jsx";
-/*const movies = ["Petr", "Lukáš", "Šimon", "Jenda"];*/
-export function Movies(){
-    return(
-        <>
-            <MovieCard imgSRC={"panprstenu.jpg"} title={"Pán Prstenů"} authors={"Peter jackson"} percentage={"100%"}/>
-            <MovieCard imgSRC={"matrix.jpg"} title={"Matrix"} authors={"Lilly Wachowski, Lana Wachowski"} percentage={"78%"}/>
-            <MovieCard imgSRC={"interstellar.jpg"} title={"Interstellar"} authors={" Christopher Nolan"} percentage={"69%"}/>
 
-{/*            {movies.map((name) => (
-                <h5>{name}</h5>
-            ))}*/}
+const movies = [{
+    imgSCR: "panprstenu.jpg",
+    title: "Pán Prstenů",
+    authors: "Peter jackson",
+    percentage: "100%"
+}, {
+    imgSCR: "matrix.jpg",
+    title: "Matrix",
+    authors: "Lilly Wachowski, Lana Wachowski",
+    percentage: "78%"
+}, {
+    imgSCR: "interstellar.jpg",
+    title: "Interstellar",
+    authors: "Christopher Nolan",
+    percentage: "69%"
+}];
+
+export function Movies() {
+    return (
+        <>
+            {movies.map((movie) => (
+                <MovieCard imgSRC={movie.imgSCR} title={movie.title} authors={movie.authors}
+                           percentage={movie.percentage}/>
+            ))}
         </>
     )
 }
