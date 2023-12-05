261822e9615734e488058ef1f32829b34f2592d5
Miraneek <surykata05@gmail.com>
2023-12-05 09:52:35 +0100

    idk

--- reacttestprosinec/src/assets/components/Movies.jsx
+++ reacttestprosinec/src/assets/components/Movies.jsx
@@ -6,6 +6,7 @@ export function Movies(){
         <>
             <MovieCard imgSRC={"panprstenu.jpg"} title={"Pán Prstenů"} authors={"Peter jackson"} percentage={"100%"}/>
             <MovieCard imgSRC={"matrix.jpg"} title={"Matrix"} authors={"Lilly Wachowski, Lana Wachowski"} percentage={"78%"}/>
+            <MovieCard imgSRC={"interstellar.jpg"} title={"Interstellar"} authors={" Christopher Nolan"} percentage={"69%"}/>
 
 {/*            {movies.map((name) => (
                 <h5>{name}</h5>
